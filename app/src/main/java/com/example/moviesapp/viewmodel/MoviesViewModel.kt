109c618196c2fa89206ebe6cb7a25673a994c3f1
hessah <hessah.mohammed.ruh.k@tuwaiq.edu.sa>
2021-11-27 19:28:11 +0300

    Edit MovieViewModel and Details Fragment

--- app/src/main/java/com/example/moviesapp/viewmodel/MoviesViewModel.kt
+++ app/src/main/java/com/example/moviesapp/viewmodel/MoviesViewModel.kt
@@ -9,21 +9,41 @@ import com.example.moviesapp.network.MovieApi
 import kotlinx.coroutines.launch
 
 
+//9
+enum class ApiStatus { LOADING , ERROR , DONE }
+
+
+
 class MoviesViewModel: ViewModel() {
+
     private var _moviesList = MutableLiveData<List<ResultsItem?>>()
     val moviesList: LiveData<List<ResultsItem?>> = _moviesList
 
+    //10
+    private val _status = MutableLiveData<ApiStatus>()
+    val status: LiveData<ApiStatus> = _status
 
+//12
+    val movieTitle = MutableLiveData<String>()
+    val movieImage = MutableLiveData<String>()
+    val movieReleaseDate = MutableLiveData<String>()
+    val movieVoteAverage = MutableLiveData<String>()
+    val movieOverview = MutableLiveData<String>()
+
+//11
     init {
         getMovies()
     }
 
-
     private fun getMovies() {
         println("Hello")
 
+
+        //6
         viewModelScope.launch {
+            //7
             try {
+                //8
                 _moviesList.value =
                     MovieApi.retrofitService.getPopularMovies().results
 
@@ -33,5 +53,17 @@ class MoviesViewModel: ViewModel() {
         }
     }
 
+
+    //13
+    fun setDetails(movieIndex: Int){
+
+        val item = _moviesList.value?.get(movieIndex)
+        movieTitle.value = item?.title
+        Log.e("TAG","title:${movieTitle.value}")
+        movieImage.value = item?.posterPath
+        movieReleaseDate.value = item?.releaseDate
+        movieVoteAverage.value = item?.voteAverage.toString()
+        movieOverview.value = item?.overview
+    }
 
 }
